641be50012c76b4819e482f3de3aca9eaacf209e
Feng-HelloWorld <52819906+Feng-HelloWorld@users.noreply.github.com>
2021-06-21 20:36:58 -0400

    重新安装ssl证书
    - 添加ssl证书；
    - 修改flask运行参数。

--- flask/main.py
+++ flask/main.py
@@ -3,4 +3,4 @@ from app.app import creat_app
 app = creat_app()
 
 if __name__=="__main__":
-    app.run(host='0.0.0.0',port=3003)
+    app.run(host='0.0.0.0',port=3003, ssl_context=('cert.pem','cert.key'))
